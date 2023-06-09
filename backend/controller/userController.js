bace10b6372cf28362bf0984387a894c4aa2383e
muskan-bajaj <muskanvijaybajaj@gmail.com>
2023-06-09 18:38:51 +0530

    week 1

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,20 +1,20 @@
 const userSchema = require("../model/userSchema");
 const jwt = require("jsonwebtoken");
 
-const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: "1d" });
+const createToken = (element) => {
+  return jwt.sign({ element }, process.env.SECRET_KEY, { expiresIn: "1d" });
 };
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await userSchema.login(email, password);
-    const jwtToken = createToken(user._id);
+    const jwtToken = createToken(user.designation);
     const id = user._id;
     res.status(200).json({
       success: true,
       user: email,
-      designation: user.designation,
+      designation: jwtToken,
       id: id,
     });
     // res.status(200).json({ email, jwtToken, id });
--- frontend/src/component/add employee/AddNewEmply.jsx
+++ frontend/src/component/add employee/AddNewEmply.jsx
@@ -1,29 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function AddNewEmply() {
+  const [employeeDetails, setEmployeeDetails] = useState({
+    name: "",
+    email: "",
+    designation: "",
+  });
+  const redirect = useNavigate();
+
+  const onChange = async (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "email") {
+      if (value.indexOf("@") === -1 || value.indexOf(".") === -1) {
+        e.target.style.border = "1px solid #FF0000";
+        e.target.style.outline = "none";
+      } else {
+        e.target.style.border = "1px solid #000000";
+      }
+    }
+    if (name === "name") {
+      if (value === "") {
+        e.target.style.border = "1px solid #FF0000";
+        e.target.style.outline = "none";
+      } else {
+        e.target.style.border = "1px solid #000000";
+      }
+    }
+
+    setEmployeeDetails({ ...employeeDetails, [name]: value });
+  };
+
+  const addEmployee = async () => {
+    const { name, email, designation } = employeeDetails;
+
+    if (
+      designation !== "" &&
+      name !== "" &&
+      email !== "" &&
+      email.indexOf("@") > -1 &&
+      email.indexOf(".") !== -1
+    ) {
+      const object = {
+        name: name,
+        email: email,
+        designation: designation,
+      };
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/admin/addEmployee",
+          object,
+          {
+            headers: { Authorization: `` },
+          }
+        );
+        console.log(res);
+
+        setEmployeeDetails({ name: "", email: "", designation: "" });
+        redirect("/admin");
+      } catch (err) {
+        console.log(err);
+        setEmployeeDetails({ name: "", email: "", designation: "" });
+      }
+    } else {
+      console.log("Fill all fields");
+    }
+  };
+
   return (
     <div className="bg-slate-200 h-[89vh] flex justify-center items-center">
       <div className="bg-slate-300 p-5 rounded">
         <div>
           <label htmlFor="name">Employee Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            name="name"
+            value={employeeDetails.name}
+            onChange={onChange}
+          />
         </div>
         <div>
           <label htmlFor="email">Employee Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            name="email"
+            value={employeeDetails.email}
+            onChange={onChange}
+          />
         </div>
         <div>
           <label htmlFor="designation">Designation</label>
-          <select>
+          <select
+            name="designation"
+            onChange={onChange}
+            value={employeeDetails.designation}
+          >
             <option hidden>--select designation--</option>
-            <option>Manager</option>
-            <option>Employee</option>
+            <option value="admin">Manager</option>
+            <option value="employee">Employee</option>
           </select>
         </div>
         <div>
           <button
             type="submit"
             className="bg-black px-3 py-1 text-white rounded-md"
+            onClick={addEmployee}
           >
             Add Employee
           </button>
